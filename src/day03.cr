da7cb5f0d2561d1eadeb02234bd50e3dbc7dd050
David L <david.landry@veldrane.de>
2022-12-07 16:47:47 +0100

    Day 03 part 2

--- src/day03.cr
+++ src/day03.cr
@@ -21,6 +21,24 @@ def find_common_priority(str : String)
   PRIORITIES[element]
 end
 
+def find_common_priority(strings)
+  first, second, third = strings
+
+  element = nil
+
+  first.each_char do |c|
+    if second.includes?(c) && third.includes?(c)
+      element = c
+      break
+    end
+  end
+
+  PRIORITIES[element]
+end
+
 result01 = rucksacks.map { |str| find_common_priority(str) }.sum
+result02 = rucksacks.in_groups_of(3, "")
+  .map { |group| find_common_priority(group) }.sum
 
 puts result01
+puts result02
