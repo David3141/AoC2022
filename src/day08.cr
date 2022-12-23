73cecbe634d21ec901e1d121e1e9fdac60b19c5b
David L <david.landry@veldrane.de>
2022-12-23 19:40:04 +0100

    Day 08 part 2 (brute-force)

--- src/day08.cr
+++ src/day08.cr
@@ -26,4 +26,42 @@ def solve_1
   count_visible
 end
 
+def count_visible(arr, n)
+  count = 0
+
+  arr.each do |elem|
+    count += 1
+    break if elem >= n
+  end
+
+  count
+end
+
+def solve_2
+  input = parse_input
+  input_transposed = input.transpose
+  max_x = input[0].size - 1
+  max_y = input.size - 1
+
+  current_max_scenic_score = 0
+
+  input.each.with_index do |row, y|
+    row.each.with_index do |tree, x|
+      # Using indexes instead of creating sub-arrays would be much more
+      # performant, I know... But this works and still is fast.
+      count_left = count_visible(row[...x].reverse, tree)
+      count_right = count_visible(row[x + 1..], tree)
+      count_top = count_visible(input_transposed[x][...y].reverse, tree)
+      count_bottom = count_visible(input_transposed[x][y + 1..], tree)
+
+      scenic_score = count_left * count_right * count_top * count_bottom
+
+      current_max_scenic_score = scenic_score if scenic_score > current_max_scenic_score
+    end
+  end
+
+  current_max_scenic_score
+end
+
 puts solve_1
+puts solve_2
